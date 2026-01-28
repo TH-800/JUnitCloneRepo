21dc313422fed86bf33cb0001c3e7b8186283d76
TH-800 <124096348+TH-800@users.noreply.github.com>
2026-01-28 14:38:46 -0500

    new project with a print method to print "Hellow world i dont know wwhat im doing"

---
+++ HelloWorld/src/main/java/come/test/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package come.test.helloworld;
+
+/**
+ *
+ * @author kaput
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World I dont know what im doing");
+    }
+}
